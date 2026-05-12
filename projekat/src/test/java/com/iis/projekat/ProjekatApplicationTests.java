5307fa73b82aad3983c1bf5f9999193b1e790224
Vladimir Rokvic <vladimirrokvic7@gmail.com>
2026-05-12 17:30:36 +0200

    First commit

---
+++ projekat/src/test/java/com/iis/projekat/ProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iis.projekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
