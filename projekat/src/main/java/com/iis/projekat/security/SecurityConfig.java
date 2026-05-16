36e32533e934df2efdfade3e602260c81df14a50
Vladimir Rokvic <vladimirrokvic7@gmail.com>
2026-05-16 17:30:00 +0200

    Ovaj commit je tu samo zato sto sam prosli uradio u pogrensom folderu

--- projekat/src/main/java/com/iis/projekat/security/SecurityConfig.java
+++ projekat/src/main/java/com/iis/projekat/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.iis.projekat.security;
 
+import com.iis.projekat.config.CorsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,16 +16,20 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private  final CorsConfig corsConfig;
 
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, CorsConfig corsConfig) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.corsConfig = corsConfig;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
 
+
         http
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
