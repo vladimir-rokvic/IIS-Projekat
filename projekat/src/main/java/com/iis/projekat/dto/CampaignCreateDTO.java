2b9c4870a2d099248942ecdefda285f93ceeb17e
Minja Knezic <sseven339@gmail.com>
2026-05-24 18:35:01 +0200

    Added campaign crud

---
+++ projekat/src/main/java/com/iis/projekat/dto/CampaignCreateDTO.java
@@ -0,0 +1,74 @@
+package com.iis.projekat.dto;
+
+import com.iis.projekat.model.CampaignStatus;
+
+import java.time.LocalDate;
+
+public class CampaignCreateDTO {
+	private String name;
+	private Double goal;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private String description;
+	private CampaignStatus status;
+	private Long projectId;
+
+	public CampaignCreateDTO() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getGoal() {
+		return goal;
+	}
+
+	public void setGoal(Double goal) {
+		this.goal = goal;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public CampaignStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(CampaignStatus status) {
+		this.status = status;
+	}
+
+	public Long getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(Long projectId) {
+		this.projectId = projectId;
+	}
+}
