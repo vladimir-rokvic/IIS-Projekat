9b584cd1bcac14734ba59aa99a2703a8d7916329
Vladimir Rokvic <vladimirrokvic7@gmail.com>
2026-06-30 23:33:20 +0200

    Yay, stigao sam i malo ekstra da uradim

--- frontend/src/pages/VolunteerDetailsPage.jsx
+++ frontend/src/pages/VolunteerDetailsPage.jsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import api from "../api/axios";
 import "./VolunteerDetailsPage.css";
+import * as Slider from '@radix-ui/react-slider';
 
 const VolunteerDetailsPage = () => {
 	const { id } = useParams();
 	const [volunteer, setVolunteer] = useState();
+
+	const DAY_LABELS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 	useEffect(() => {
 		const fetchVolunteer = async () => {
@@ -32,38 +35,71 @@ const VolunteerDetailsPage = () => {
 
 			<label className="addressLabelInre">Basic information</label>
 			<div className="vp-basic-info">
-				<div>
+				<div style={{display: 'flex', gap: '100px'}}>
 					<div>
-					<h2 style={{marginTop: '10px'}}>Name</h2>
-					<p>{volunteer.name} {volunteer.surname}</p>
-					<h2>Contact information</h2>
-					<p>Email: {volunteer.email}</p>
-					<p>Phone: {volunteer.phone}</p>
-					<h2>Address</h2>
-					{(volunteer.address !== null) ? (
-					<div style={{display: 'flex', gap: '50px'}}>
-						<div>
-							<p style={{fontSize: '1.3rem'}}>Country</p>
-							<p>{volunteer.address.country}</p>
-						</div>
-						<div>
-							<p style={{fontSize: '1.3rem'}}>City</p>
-							<p>{volunteer.address.city}</p>
-						</div>
-						<div>
-							<p style={{fontSize: '1.3rem'}}>Street</p>
-							<p>{volunteer.address.street}</p>
-						</div>
-					</div>) : (<p style={{color: '#555', fontSize: '1.3rem'}}>No address provided</p>)}
-					<h2>Date of birth</h2>
-					<p>{volunteer.dateOfBirth}</p>
-					<p>Age: {calculateAge(volunteer)}</p>
+						<h2 style={{marginTop: '10px'}}>Name</h2>
+						<p>{volunteer.name} {volunteer.surname}</p>
+						<h2>Contact information</h2>
+						<p>Email: {volunteer.email}</p>
+						<p>Phone: {volunteer.phone}</p>
+						<h2>Address</h2>
+						{(volunteer.address !== null) ? (
+						<div style={{display: 'flex', gap: '50px'}}>
+							<div>
+								<p style={{fontSize: '1.3rem'}}>Country</p>
+								<p>{volunteer.address.country}</p>
+							</div>
+							<div>
+								<p style={{fontSize: '1.3rem'}}>City</p>
+								<p>{volunteer.address.city}</p>
+							</div>
+							<div>
+								<p style={{fontSize: '1.3rem'}}>Street</p>
+								<p>{volunteer.address.street}</p>
+							</div>
+						</div>) : (<p style={{color: '#555', fontSize: '1.3rem'}}>No address provided</p>)}
+						<h2>Date of birth</h2>
+						<p>{volunteer.dateOfBirth}</p>
+						<p>Age: {calculateAge(volunteer)}</p>
 
-					<h2>Biography</h2>
-					{(volunteer.bio) ? 
-						(<p className="vp-bio-section">{volunteer.bio}</p>)
-						: (<p style={{color: '#555'}}>No bio given</p>)
-					}
+						<h2>Biography</h2>
+						{(volunteer.bio) ? 
+							(<p className="vp-bio-section">{volunteer.bio}</p>)
+							: (<p style={{color: '#555'}}>No bio given</p>)
+						}
+					</div>
+					<div className='v-availability-dash'>
+						{volunteer.availabilities && 
+						(volunteer.availabilities.map(a => (
+							<div key={a.id} className="day-card"
+								style={{width: '300px', height: '200px'}}>
+                            	<h2>{a.day}</h2>
+                            {a.enabled && (
+                                <>
+                                    <div style={{display: 'flex', justifyContent: 'space-between', marginBottom: '15px'}}>
+                                        <p>{a.startHour}:00</p>
+                                        <p>{a.endHour}:00</p>
+                                    </div>
+                                    <Slider.Root
+                                        className="slider-root"
+                                        min={7}
+                                        max={22}
+                                        step={1}
+                                        value={[a.startHour, a.endHour]}
+										disabled
+										style={{pointerEvents: 'none'}}
+                                    >
+                                        <Slider.Track className="slider-track">
+                                            <Slider.Range className="slider-range" />
+                                        </Slider.Track>
+                                        <Slider.Thumb className="slider-thumb" />
+                                        <Slider.Thumb className="slider-thumb" />
+                                    </Slider.Root>
+                                </>
+                            )}
+							</div>
+						)
+						))}
 					</div>
 				</div>
 			</div>
